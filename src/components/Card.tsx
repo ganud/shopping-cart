47c6965b05c0a564648e52244eb6055233c473e5
Dundo <107737568+ganud@users.noreply.github.com>
2024-04-26 12:10:46 -0400

    Set up fetch on shop page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,17 +4,9 @@ export default function Card({ image, title, price }) {
   return (
     <>
       <div className={classes.container}>
-        <img
-          src={
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Banana.png/836px-Banana.png"
-          }
-          alt=""
-        />
-        <h1>Price: $420.69</h1>
-        <h2>
-          Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA
-          III 2.5
-        </h2>
+        <img src={`${image}`} alt="" />
+        <h1>Price: {price}</h1>
+        <h2>{title}</h2>
         <div className={classes.centered}>
           <button>Add to Cart</button>
         </div>
--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,13 +1,35 @@
 import Card from "./Card";
 // Fetch and return an array of cards listing products
 import classes from "./Shop.module.css";
-
+import { useEffect, useState } from "react";
 export default function Shop() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/")
+      .then((res) => res.json())
+      .then((json) =>
+        json.filter(
+          (item) =>
+            item.category === "men's clothing" ||
+            item.category === "women's clothing"
+        )
+      )
+      .then((filtered) => {
+        setProducts(filtered);
+      });
+  }, []);
+
   return (
     <>
       <div className={classes.container}>
-        <Card></Card>
-        <Card></Card>
+        {products.map((product) => (
+          <Card
+            image={product.image}
+            title={product.title}
+            price={product.price}
+          ></Card>
+        ))}
       </div>
     </>
   );
