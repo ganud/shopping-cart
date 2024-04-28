82f9dd53bc4441a5b4373cf56e5a6347eff92c5d
Dundo <107737568+ganud@users.noreply.github.com>
2024-04-28 12:20:08 -0400

    Make fetch run only once

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -7,26 +7,25 @@ import { useOutletContext } from "react-router-dom";
 export default function Shop() {
   const [products, setProducts] = useOutletContext();
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/")
-      .then((res) => res.json())
-      .then((json) =>
-        json.filter(
-          (item: { category: string }) =>
-            item.category === "men's clothing" ||
-            item.category === "women's clothing"
+    if (products.length === 0) {
+      fetch("https://fakestoreapi.com/products/")
+        .then((res) => res.json())
+        .then((json) =>
+          json.filter(
+            (item: { category: string }) =>
+              item.category === "men's clothing" ||
+              item.category === "women's clothing"
+          )
         )
-      )
-      .then((filtered) => {
-        // Add a quantity value for the cart
-        let filteredit = filtered.map((product) => {
-          if (getQuantity(products) !== 0) {
-            // Check if there already products in the cart
-            return product;
-          }
-          return { ...product, quantity: 0 }; // By default, all quantiies are zero
+        .then((filtered) => {
+          // Add a quantity value for the product selection
+          let filteredit = filtered.map((product) => {
+            return { ...product, quantity: 0 };
+          });
+          setProducts(filteredit);
+          console.log(products);
         });
-        setProducts(filteredit);
-      });
+    }
   }, []);
 
   return (
